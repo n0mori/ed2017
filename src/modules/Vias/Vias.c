58f9c23358139ce7650ef94a296e0238351d9263
nick <nick@nick-deb>
2018-02-06 12:19:50 -0200

    nothing worthy

--- src/modules/Vias/Vias.c
+++ src/modules/Vias/Vias.c
@@ -4,11 +4,16 @@ typedef struct vias {
   Grafo g;
 }* StVias;
 
+int cmpt(void *a, void *b) {
+  return 1;
+}
+
 Vias new_vias() {
   StVias v = malloc(sizeof(struct vias));
   v->g = new_grafo();
   return v;
 }
+
 void vias_insert_esquina(Vias v, char *id, double x, double y) {
   StVias vias = (StVias) v;
   Ponto esquina = new_ponto(x, y);
@@ -21,4 +26,15 @@ void vias_insert_rua(Vias v, char *from, char *to, char *ldir, char *lesq, doubl
   grafo_insert_edge(vias->g, from, to, rua);
 }
 
-Lista vias_calcular_rota(Vias v, Ponto inicio, Ponto fim, int option);
+Vertex vias_nearest(Lista list, Ponto p) {
+
+}
+
+Lista vias_calcular_rota(Vias v, Ponto inicio, Ponto fim, int option) {
+  Lista rota = create_lista();
+  Lista unvisited = create_lista();
+  Grafo g = ((StVias) v);
+
+
+
+}
